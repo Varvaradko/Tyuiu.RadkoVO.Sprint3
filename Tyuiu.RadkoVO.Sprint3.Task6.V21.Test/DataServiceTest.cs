58cfd1dd6ac8bb2cc6e8ab51c09e1cc1f27c3a50
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-13 12:01:03 +0500

    Task6

---
+++ Tyuiu.RadkoVO.Sprint3.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task6.V21.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task6.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 19;
+            int stopValue = 30;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 485;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint3.Task6.V21/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task6.V21.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Радько В. О. | СМАРТб-25-1";
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Радько Варвара Олеговна | СМАРТб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [19, 30] сумму всех делителей.                        *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 19;
+            int stopValue = 30;
+
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
