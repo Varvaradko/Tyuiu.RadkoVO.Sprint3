626c0f0348df934c2ef475980621772ac4723a43
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-13 08:27:38 +0500

    Task4

---
+++ Tyuiu.RadkoVO.Sprint3.Task4.V15.Task/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task4.V15.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task4.V15.Task
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(2.591, ds.Calculate(-1, 0));
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint3.Task4.V15/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task4.V15.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("y=((sin(x)+x)/x)+0.75,  -5 <= x <= 5");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(-5, 5));
+            Console.ReadKey();
+        }
+    }
+}
