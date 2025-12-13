b203286828f52d88dda6fdd5fa605c2d77d5bd79
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-13 08:00:08 +0500

    Task2

---
+++ Tyuiu.RadkoVO.Sprint3.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task2.V8.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task2.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            double wait = 0.001;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint3.Task2.V8/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.RadkoVO.Sprint3.Task2.V8.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint3.Task2.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Радько В. О. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Радько Варвара Олеговна | СМАРТб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая                   *");
+            Console.WriteLine("* вычисляет сумму ряда по формуле                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 1;
+            int stopValue = 15;
+
+            Console.WriteLine("Стар шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
